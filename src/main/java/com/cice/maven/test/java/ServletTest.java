c4306c66383cbab96db497d79ab2d89f8ebd35d8
nyeke <nyeke@hotmail.com>
2018-09-19 21:09:36 +0200

    mysql

--- src/main/java/com/cice/maven/test/java/ServletTest.java
+++ src/main/java/com/cice/maven/test/java/ServletTest.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 
-public class ServletTest extends HttpServlet {
+public class    ServletTest extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
